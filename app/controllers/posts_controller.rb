5ed17ca8abbf6409649ce812b5d58c6ea8e561bf
“peifen-wu” <wuplaystation@gmail.com>
2016-06-30 16:12:14 +0800

    adding relationship for each individual user  will have many post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,15 +7,17 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
     end
 
     def new
-        @post = Post.new
+        @post = current_user.posts.build
     end
 
     def create
-        if @post = Post.create(post_params)
+        @post = current_user.posts.build(post_params)
+
+        if @post.save
             flash[:success] = "Your post has been created!"
             redirect_to posts_path
         else
-            flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+            flash[:alert] = "Your new post couldn't be created!  Please check the form."
             render :new
         end
     end
